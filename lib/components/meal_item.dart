0a28a2a16e95123c79c27647b0227f0ae78d58f4
Gabriel <gabriel.avlis.123@gmail.com>
2023-07-20 10:33:49 -0300

    first commit

---
+++ lib/components/meal_item.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import '../models/meals.dart';
+
+class MealItem extends StatelessWidget {
+  final Meal meal;
+
+  void _selectMeal() {}
+
+  const MealItem(this.meal, {super.key});
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: _selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 5,
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  right: 10,
+                  bottom: 20,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      meal.title,
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true, //quebra de linha
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(children: [
+                Icon(Icons.schedule),
+                SizedBox(width: 7),
+                Text('${meal.duration} min')
+              ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
